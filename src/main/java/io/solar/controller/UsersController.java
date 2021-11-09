3e4d41142b102381cf16b1ca72b5d3cbffb33468
K4kTyC <47088033+K4kTyC@users.noreply.github.com>
2021-11-09 19:41:33 +0300

    Rewrite UsersController using SpringBoot

--- src/main/java/io/solar/controller/UsersController.java
+++ src/main/java/io/solar/controller/UsersController.java
@@ -1,30 +1,26 @@
 package io.solar.controller;
 
 import io.solar.entity.User;
-import io.solar.mapper.TotalMapper;
-import io.solar.mapper.UserMapper;
-import io.solar.utils.Page;
-import io.solar.utils.QueryUtils;
+import io.solar.service.UserService;
 import io.solar.utils.context.AuthData;
-import io.solar.utils.db.Query;
 import io.solar.utils.db.Transaction;
 import io.solar.utils.server.Pageable;
-import io.solar.utils.server.beans.Controller;
-import io.solar.utils.server.controller.PathVariable;
-import io.solar.utils.server.controller.RequestBody;
-import io.solar.utils.server.controller.RequestMapping;
-import io.solar.utils.server.controller.RequestParam;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-@Component
-@Controller
+@RestController
 @RequestMapping("users")
 public class UsersController {
 
+    private final UserService userService;
 
-    @RequestMapping
+    @Autowired
+    public UsersController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
     public Page<User> getList(
             Pageable pageable,
             Transaction transaction,
@@ -32,79 +28,24 @@ public class UsersController {
             @RequestParam("login") String login,
             @RequestParam("title") String title
     ) {
-        boolean canEdit = AuthController.userCan(user, "edit-user", transaction);
-        if(!canEdit) {
-            login = null;
-        } else if("".equals(login)) {
-            login = null;
-        }
-        if("".equals(title)) {
-            title = null;
-        }
-
-        Query dataQuery = transaction.query("select * from users where 1 = 1 "
-                + (login != null ? " and login like :login" : "")
-                + (title != null ? " and title like :title" : "") + " limit :skip, :pageSize");
-
-        Query countQuery = transaction.query("select count(1) from users where 1 = 1 "
-                       + (login != null ? " and login like :login" : "")
-                       + (title != null ? " and title like :title" : ""));
-        if(login != null) {
-            dataQuery.setString("login", login + "%");
-            countQuery.setString("login", login + "%");
-        }
-        if(title != null) {
-            dataQuery.setString("title", title + "%");
-            countQuery.setString("title", title + "%");
-        }
-        QueryUtils.applyPagination(dataQuery, pageable);
-        List<User> data = mapUsers(dataQuery.executeQuery(new UserMapper()), canEdit);
-
-        long count = countQuery.executeQuery(new TotalMapper()).get(0);
-        return new Page<>(data, count);
+        return userService.getAllUsers(user, transaction, login, title, pageable);
     }
 
-    @RequestMapping("{id}")
+    @GetMapping("{id}")
     public User getOne(@PathVariable("id") Long id, Transaction transaction, @AuthData User user) {
-        Query query = transaction.query("select * from users where id = :id");
-        query.setLong("id", id);
-        boolean canEdit = AuthController.userCan(user, "edit-user", transaction);
-        List<User> users = mapUsers(query.executeQuery(new UserMapper()), canEdit);
-        return users.size() == 1 ? users.get(0) : null;
+        return userService.getUserById(id, transaction, user);
     }
 
-    @RequestMapping(method = "post")
+    @PostMapping
     public User updateUser(@AuthData User user, @RequestBody User payload, Transaction transaction) {
-        if(!((user.getId() != null && user.getId().equals(payload.getId())) || AuthController.userCan(user, "edit-user", transaction))) {
+        boolean canEdit = AuthController.userCan(user, "edit-user", transaction);
+        if (!((user.getId() != null && user.getId().equals(payload.getId())) || canEdit)) {
             throw new RuntimeException("no permissions");
         }
-        if(payload.getId() == null) {
+        if (payload.getId() == null) {
             throw new RuntimeException("bad request, id should be defined");
         }
-        Query query = transaction.query("update users set title = :title where id = :id");
-        query.setString("title", payload.getTitle());
-        query.setLong("id", payload.getId());
-        query.execute();
 
-        query = transaction.query("select * from users where id = :id");
-        query.setLong("id", payload.getId());
-
-        return mapUsers(query.executeQuery(new UserMapper()), true).get(0);
+        return userService.updateUserTitle(payload.getId(), payload.getTitle(), canEdit);
     }
-
-    private List<User> mapUsers(List<User> users, boolean canEdit) {
-        for(User user : users) {
-            if(user.getTitle() == null || user.getTitle().equals("")) {
-                String login = user.getLogin();
-                int index = login.indexOf("@");
-                user.setTitle(index > -1 ? login.substring(0, user.getLogin().indexOf("@")) : login);
-            }
-            user.setPassword("");
-            if(!canEdit) {
-                user.setLogin("");
-            }
-        }
-        return users;
-    }
-
 }
--- src/main/java/io/solar/service/UserService.java
+++ src/main/java/io/solar/service/UserService.java
@@ -1,14 +1,20 @@
 package io.solar.service;
 
+import io.solar.controller.AuthController;
 import io.solar.entity.User;
 import io.solar.repository.UserRepository;
+import io.solar.utils.db.Transaction;
+import io.solar.utils.server.Pageable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Service
@@ -43,4 +49,70 @@ public class UserService implements UserDetailsService {
         return passwordEncoder.matches(userFromUI.getPassword(), userFromDb.getPassword());
     }
 
+    public Page<User> getAllUsers(User user, Transaction transaction, String login, String title, Pageable pageable) {
+        boolean canEdit = AuthController.userCan(user, "edit-user", transaction);
+        if(!canEdit) {
+            login = null;
+        } else if("".equals(login)) {
+            login = null;
+        }
+        if("".equals(title)) {
+            title = null;
+        }
+
+        PageRequest paging = PageRequest.of(pageable.getPage(), pageable.getPageSize());
+        Page<User> users;
+        if (login != null && title != null) {
+            users = userRepository.findByLoginStartingWithAndTitleStartingWith(paging, login, title);
+        } else if (login != null) {
+            users = userRepository.findByLoginStartingWith(paging, login);
+        } else if (title != null) {
+            users = userRepository.findByTitleStartingWith(paging, title);
+        } else {
+            users = userRepository.findAll(paging);
+        }
+
+        users.map(u -> mapUser(user, canEdit));
+
+        return users;
+    }
+
+    public User getUserById(Long id, Transaction transaction, User userData) {
+        boolean canEdit = AuthController.userCan(userData, "edit-user", transaction);
+        User user;
+
+        try {
+            user = userRepository.findById(id).get();
+        } catch (NoSuchElementException e) {
+            return null;
+        }
+
+        return mapUser(user, canEdit);
+    }
+
+    public User updateUserTitle(Long id, String title, boolean canEdit) {
+        User user;
+        try {
+            user = userRepository.findById(id).get();
+        } catch (NoSuchElementException e) {
+            throw new RuntimeException("no user with such id");
+        }
+
+        user.setTitle(title);
+        user = userRepository.save(user);
+        return mapUser(user, canEdit);
+    }
+
+    private User mapUser(User user, boolean canEdit) {
+        if (user.getTitle() == null || user.getTitle().equals("")) {
+            String log = user.getLogin();
+            int index = log.indexOf("@");
+            user.setTitle(index > -1 ? log.substring(0, user.getLogin().indexOf("@")) : log);
+        }
+        user.setPassword("");
+        if (!canEdit) {
+            user.setLogin("");
+        }
+        return user;
+    }
 }
