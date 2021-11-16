87b444b0d4705031ac33d80375db85ec2f76ec05
Aliaksei Koran <akoran@scnsoft.com>
2021-11-16 16:56:12 +0300

    @EnableGlobalMethodSecurity(prePostEnabled = true)

--- src/main/java/io/solar/config/SecurityConfig.java
+++ src/main/java/io/solar/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import io.solar.security.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,6 +16,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private JwtFilter jwtFilter;
