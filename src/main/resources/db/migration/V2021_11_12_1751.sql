e37aa02bc6fa8bc6529298f6b008f11b5b74e5df
Aliaksei Koran <akoran@scnsoft.com>
2021-11-17 11:29:12 +0300

    new migration

--- src/main/resources/db/migration/V2021_11_12_1751.sql
+++ src/main/resources/db/migration/V2021_11_12_1751.sql
@@ -1,1 +1,2 @@
 ALTER TABLE user modify hack_attempts int default 0 not null;
+
---
+++ src/main/resources/db/migration/V2021_11_17_1124.sql
@@ -0,0 +1,4 @@
+INSERT INTO solar.permission_type (id, title)
+VALUES
+    (1, 'play-the-game'),
+    (2, 'edit-the-game');
