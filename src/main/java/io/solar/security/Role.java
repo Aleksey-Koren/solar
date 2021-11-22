e1e6a3697f6a0899ac4817d8945d509472ec4b9d
K4kTyC <47088033+K4kTyC@users.noreply.github.com>
2021-11-22 14:43:55 +0300

    Update permissions

--- src/main/java/io/solar/security/Role.java
+++ src/main/java/io/solar/security/Role.java
@@ -5,7 +5,7 @@ import java.util.Set;
 public enum Role {
 
     USER(Set.of("PLAY_THE_GAME")),
-    ADMIN(Set.of("EDIT_PLANET", "EDIT_USER", "EDIT_PERMISSION", "ASSIGN_PERMISSION", "REVOKE_PERMISSION"));
+    ADMIN(Set.of("EDIT_PLANET", "EDIT_USER", "SEE_PERMISSIONS", "ASSIGN_PERMISSIONS"));
 
     Role(Set<String> permissions) {
         this.permissions = permissions;
