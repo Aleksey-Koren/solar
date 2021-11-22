2cc4f661f62ff1f5f48b7571580c78c84d36d39b
K4kTyC <47088033+K4kTyC@users.noreply.github.com>
2021-11-22 14:37:14 +0300

    Merge fix

--- src/main/java/io/solar/controller/UsersController.java
+++ src/main/java/io/solar/controller/UsersController.java
@@ -12,8 +12,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 import java.util.List;
