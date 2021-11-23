d185d04acd0df181abe34e8496dc96e202a7f073
Aliaksei Koran <akoran@scnsoft.com>
2021-11-23 17:53:23 +0300

    Refactor ProductController

--- src/main/java/io/solar/controller/ProductController.java
+++ src/main/java/io/solar/controller/ProductController.java
@@ -1,153 +1,76 @@
 package io.solar.controller;
 
-import io.solar.entity.Product;
-import io.solar.entity.User;
-import io.solar.mapper.ProductMapper;
-import io.solar.mapper.TotalMapper;
+import io.solar.dto.ProductDto;
+import io.solar.facade.ProductFacade;
+import io.solar.service.ProductService;
 import io.solar.utils.Option;
-import io.solar.utils.Page;
-import io.solar.utils.QueryUtils;
-import io.solar.utils.context.AuthData;
-import io.solar.utils.db.Query;
-import io.solar.utils.db.Transaction;
-import io.solar.utils.server.Pageable;
-import io.solar.utils.server.controller.PathVariable;
-import io.solar.utils.server.controller.RequestBody;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
+
+import static java.util.stream.Collectors.*;
 
 @Slf4j
 @RestController
 @RequestMapping(value = "/api/product")
 public class ProductController {
 
+    private ProductFacade productFacade;
+    private ProductService productService;
+
+
+    @Autowired
+    public ProductController(ProductFacade productFacade, ProductService productService) {
+        this.productFacade = productFacade;
+        this.productService = productService;
+    }
+
+    @Transactional
     @PreAuthorize("hasAuthority('EDIT_PRODUCT')")
     @PostMapping
-    public Product save(@RequestBody Product product, @AuthData User user, Transaction transaction) {
-        Query save = null;
-        if (product.getId() != null) {
-            Query query = transaction.query("select * from products where id = :id");
-            query.setLong("id", product.getId());
-            List<Product> existing = null;
-
-            if (existing.size() == 1) {
-                save = transaction.query("UPDATE products set title=:title,image=:image,bulk=:bulk," +
-                        "mass=:mass,price=:price where id=:id");
-                save.setLong("id", product.getId());
-            } else {
-                log.error("can't find planet with id: " + product.getId());
-            }
-        } else {
-            save = transaction.query("insert into products (title, image, bulk, mass, price)" +
-                    " values (:title, :image, :bulk, :mass, :price)");
-        }
-        if (save != null) {
-            save.setString("title", product.getTitle());
-            save.setString("image", product.getImage());
-            save.setFloat("bulk", product.getBulk());
-            save.setFloat("mass", product.getMass());
-            save.setFloat("price", product.getPrice());
-
-            save.execute();
-            if (product.getId() == null) {
-                product.setId(save.getLastGeneratedKey(Long.class));
-            }
-        } else {
-            throw new RuntimeException("Can't save or update product");
-        }
-        return product;
+    public ResponseEntity<ProductDto> save(@RequestBody ProductDto dto) {
+        return ResponseEntity.ok().body(productFacade.save(dto));
     }
 
-
-
-
-//    @PreAuthorize("hasAuthority('EDIT_PRODUCT')")
-//    @PostMapping
-//    public Product save(@RequestBody Product product, @AuthData User user, Transaction transaction) {
-//        if (!AuthController.userCan(user, "edit-product", transaction)) {
-//            throw new RuntimeException("no privileges");
-//        }
-//        Query save = null;
-//        if (product.getId() != null) {
-//            Query query = transaction.query("select * from products where id = :id");
-//            query.setLong("id", product.getId());
-//            List<Product> existing = query.executeQuery(new ProductMapper());
-//
-//            if (existing.size() == 1) {
-//                save = transaction.query("UPDATE products set title=:title,image=:image,bulk=:bulk," +
-//                        "mass=:mass,price=:price where id=:id");
-//                save.setLong("id", product.getId());
-//            } else {
-//                log.error("can't find planet with id: " + product.getId());
-//            }
-//        } else {
-//            save = transaction.query("insert into products (title, image, bulk, mass, price)" +
-//                    " values (:title, :image, :bulk, :mass, :price)");
-//        }
-//        if (save != null) {
-//            save.setString("title", product.getTitle());
-//            save.setString("image", product.getImage());
-//            save.setFloat("bulk", product.getBulk());
-//            save.setFloat("mass", product.getMass());
-//            save.setFloat("price", product.getPrice());
-//
-//            save.execute();
-//            if (product.getId() == null) {
-//                product.setId(save.getLastGeneratedKey(Long.class));
-//            }
-//        } else {
-//            throw new RuntimeException("Can't save or update product");
-//        }
-//        return product;
-//    }
-
-    @RequestMapping("{id}")
-    public Product get(@PathVariable("id") Long id, Transaction transaction) {
-        Query query = transaction.query("select * from products where id = :id");
-        query.setLong("id", id);
-        List<Product> existing = null;
-        return existing.size() == 1 ? existing.get(0) : null;
+    //TODO I didn't see any fields fo filtration or searching on UI.
+    // We should decide if we do filtration at this endpoint.
+    @Transactional
+    @PreAuthorize("hasAnyAuthority('PLAY_THE_GAME', 'EDIT_PRODUCT')")
+    @GetMapping
+    public ResponseEntity<Page<ProductDto>> getAll(@PageableDefault Pageable pageable) {
+        return ResponseEntity.ok().body(productFacade.findAll(pageable));
     }
 
-
-    @RequestMapping
-    public Page<Product> getAll(Pageable pageable, Transaction transaction) {
-        Query countQ = transaction.query("select count(1) from products");
-        Long count = countQ.executeQuery(new TotalMapper()).get(0);
-        if(count == 0) {
-            return Page.empty();
-        }
-
-        Query query = transaction.query("select * from products limit :skip, :pageSize");
-        QueryUtils.applyPagination(query, pageable);
-        List<Product> existing = null;
-        return new Page<>(existing, count);
+    @Transactional
+    @PreAuthorize("hasAnyAuthority('PLAY_THE_GAME', 'EDIT_PRODUCT')")
+    @GetMapping("{id}")
+    public ResponseEntity<ProductDto> get(@PathVariable("id") Long id) {
+        Optional<ProductDto> dto = productFacade.findById(id);
+        return dto.isPresent() ? ResponseEntity.ok(dto.get()) : ResponseEntity.notFound().build();
     }
 
-    @RequestMapping("utils/dropdown")
-    public List<Option> dropdown(Transaction transaction) {
-        return getAll(new Pageable(0, 9999999), transaction).getContent()
+    @Transactional
+    @GetMapping("utils/dropdown")
+    public List<Option> dropdown() {
+        return productService.findAll()
                 .stream()
                 .map(v -> new Option(v.getId(), v.getTitle()))
-                .collect(Collectors.toList());
+                .collect(toList());
     }
 
-
-    @RequestMapping(value = "{id}")
-    public void delete(@PathVariable("id") Long id, @AuthData User user, Transaction transaction) {
-        if (!AuthController.userCan(user, "edit-product", transaction)) {
-            throw new RuntimeException("no privileges");
-        }
-        Query query = transaction.query("delete from products where id = :id");
-        query.setLong("id", id);
-        query.execute();
+    @Transactional
+    @PreAuthorize("hasAuthority('EDIT_PRODUCT')")
+    @DeleteMapping(value = "{id}")
+    public void delete(@PathVariable("id") Long id) {
+        productService.deleteById(id);
     }
-
-
 }
--- src/main/java/io/solar/service/ProductService.java
+++ src/main/java/io/solar/service/ProductService.java
@@ -3,8 +3,13 @@ package io.solar.service;
 import io.solar.entity.Product;
 import io.solar.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,5 +24,21 @@ public class ProductService {
 
     public Optional<Product> findById (Long id) {
         return productRepository.findById(id);
+    }
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Page<Product> findAll( Pageable pageable) {
+        return productRepository.findAll(pageable);
+    }
+
+    public void deleteById(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
     }
 }
