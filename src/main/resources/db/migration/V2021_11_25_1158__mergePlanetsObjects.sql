60d0633a3c0df968ecce4355332dbc619de958d8
Aliaksei Koran <akoran@scnsoft.com>
2021-11-26 14:03:37 +0300

    Copy planets to objects

--- src/main/resources/db/migration/V2021_11_25_1158__mergePlanetsObjects.sql
+++ src/main/resources/db/migration/V2021_11_25_1158__mergePlanetsObjects.sql
@@ -7,3 +7,18 @@ AlTER TABLE objects
 INSERT INTO object_types (title)
 VALUES
     ('planet');
+
+ALTER TABLE planet ADD COLUMN status ENUM ('in_space', 'attached_to', 'in_container');
+
+UPDATE planet
+SET status = 'in_space';
+
+
+ALTER TABLE planet
+    RENAME TO planets;
+
+INSERT INTO objects (aphelion, orbital_period, angle, title, status)
+SELECT aphelion, orbital_period, angle, title, status FROM planets;
+
+ALTER TABLE planets
+    DROP COLUMN status;
