06055778a5133f256e40e2ccbad00e50c8f20741
Aliaksei Koran <akoran@scnsoft.com>
2021-12-06 12:47:02 +0300

    import

--- src/main/java/io/solar/entity/objects/BasicObject.java
+++ src/main/java/io/solar/entity/objects/BasicObject.java
@@ -4,19 +4,7 @@ import io.solar.entity.Planet;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.List;
 
 @Data
