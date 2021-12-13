68a20e904337fdf937ec527e1b1b1afd8940bb4e
Aliaksei Koran <akoran@scnsoft.com>
2021-12-13 14:34:35 +0300

    Add deleteAvatar method

--- src/main/java/io/solar/controller/ImageUploadController.java
+++ src/main/java/io/solar/controller/ImageUploadController.java
@@ -1,14 +1,12 @@
 package io.solar.controller;
 
 import io.solar.dto.ImageDto;
+import io.solar.service.image.DocumentService;
 import io.solar.service.image.ImageUploadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,11 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class ImageUploadController {
 
     private final ImageUploadService imageUploadService;
+    private final DocumentService documentService;
 
     @Transactional
     @PreAuthorize("hasAnyAuthoriy('EDIT_USER', 'PLAY_THE_GAME')")
     @PostMapping
     public void uploadAvatar(@RequestBody ImageDto imageDto) {
         imageUploadService.uploadAvatar(imageDto);
+    }
+
+    @DeleteMapping("{id}")
+    @PreAuthorize("hasAnyAuthoriy('EDIT_USER', 'PLAY_THE_GAME')")
+    @Transactional
+    public void deleteAvatar(@PathVariable("id") Long id) {
+        documentService.deleteAvatarByUserId(id);
     }
 }
