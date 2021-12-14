c5ab46930075abe14fa1da7ded7fe7e906454437
Vyshamirski <avyshamirski@scnsoft.com>
2021-12-14 19:09:58 +0300

    fix: remove call login endpoint before each test

--- src/test/java/io/solar/controller/ProductControllerTest.java
+++ src/test/java/io/solar/controller/ProductControllerTest.java
@@ -1,12 +1,9 @@
 package io.solar.controller;
 
 import io.solar.dto.ProductDto;
-import io.solar.dto.Token;
-import io.solar.dto.UserDto;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.type.TypeReference;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -16,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -32,37 +30,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class ProductControllerTest {
 
     private final String PRODUCT_CONTROLLER_URL = "/api/product/";
-    private final String AUTH_TOKEN_HEADER_NAME = "auth_token";
 
     @Autowired
     private MockMvc mockMvc;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    private static String authToken;
     private static String savedProductId;
 
-    @BeforeEach
-    public void getAuthToken() throws Exception {
-
-        UserDto userDto = new UserDto();
-        userDto.setLogin("admin");
-        userDto.setPassword("admin");
-
-        MvcResult mvcResult = mockMvc
-                .perform(post("/api/login")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(userDto))
-                )
-                .andExpect(status().isOk())
-                .andReturn();
-
-        Token token = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), Token.class);
-
-        authToken = token.getData();
-    }
-
     @Test
-    @Order(1)
+    @WithMockUser(authorities = {"PLAY_THE_GAME", "EDIT_PRODUCT"})
     @Disabled
     public void getAllProducts_pageable_notEmptyPage() throws Exception {
 
@@ -72,26 +48,23 @@ public class ProductControllerTest {
 
         MvcResult mvcResult = mockMvc
                 .perform(get(PRODUCT_CONTROLLER_URL)
-                        .params(queryParams)
-                        .header(AUTH_TOKEN_HEADER_NAME, authToken))
+                        .params(queryParams))
                 .andExpect(status().isOk())
                 .andReturn();
 
-        Page<ProductDto> productDtoList = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), new TypeReference<>() {
-        });
+        Page<ProductDto> productDtoList = objectMapper.readValue(mvcResult.getResponse().getContentAsString(), new TypeReference<>() {});
 
         Assertions.assertEquals(10, productDtoList.getTotalElements());
     }
 
     @Test
-    @Order(2)
+    @WithMockUser(authorities = {"PLAY_THE_GAME", "EDIT_PRODUCT"})
     public void getProduct_productId_notEmptyResponse() throws Exception {
 
         String productId = "15";
 
         MvcResult mvcResult = mockMvc
                 .perform(get(PRODUCT_CONTROLLER_URL.concat(productId))
-                        .header(AUTH_TOKEN_HEADER_NAME, authToken)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andReturn();
@@ -102,7 +75,7 @@ public class ProductControllerTest {
     }
 
     @Test
-    @Order(3)
+    @WithMockUser(authorities = {"PLAY_THE_GAME", "EDIT_PRODUCT"})
     public void saveProduct_productDto_statusCodeIsOk() throws Exception {
 
         ProductDto productDto = ProductDto.builder()
@@ -112,7 +85,6 @@ public class ProductControllerTest {
 
         mockMvc
                 .perform(post(PRODUCT_CONTROLLER_URL)
-                        .header(AUTH_TOKEN_HEADER_NAME, authToken)
                         .content(objectMapper.writeValueAsString(productDto))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -120,8 +92,7 @@ public class ProductControllerTest {
                     ProductDto resp = objectMapper.readValue(res.getResponse().getContentAsString(), ProductDto.class);
 
                     mockMvc
-                            .perform(get(PRODUCT_CONTROLLER_URL.concat(resp.getId().toString()))
-                                    .header(AUTH_TOKEN_HEADER_NAME, authToken))
+                            .perform(get(PRODUCT_CONTROLLER_URL.concat(resp.getId().toString())))
                             .andExpect(status().isOk());
 
                     savedProductId = resp.getId().toString();
@@ -129,23 +100,21 @@ public class ProductControllerTest {
     }
 
     @Test
-    @Order(4)
+    @WithMockUser(authorities = {"PLAY_THE_GAME", "EDIT_PRODUCT"})
     public void deleteProduct_productId_statusCodeIsOk() throws Exception {
 
         mockMvc
                 .perform(delete(PRODUCT_CONTROLLER_URL.concat(savedProductId))
-                        .header(AUTH_TOKEN_HEADER_NAME, authToken)
                         .accept(MediaType.APPLICATION_JSON)
                 )
                 .andExpect(status().isOk());
     }
 
     @Test
-    @Order(5)
+    @WithMockUser(authorities = {"PLAY_THE_GAME", "EDIT_PRODUCT"})
     public void dropdown_statusCodeIsOk() throws Exception {
         mockMvc
                 .perform(get(PRODUCT_CONTROLLER_URL.concat("utils/dropdown"))
-                        .header(AUTH_TOKEN_HEADER_NAME, authToken)
                         .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
