95d2628669aa68bb72ed488b9c42746d13f65291
Aliaksei Koran <akoran@scnsoft.com>
2021-12-15 17:55:38 +0300

    PlanetControllerTest finished

--- src/main/java/io/solar/security/JwtFilter.java
+++ src/main/java/io/solar/security/JwtFilter.java
@@ -1,7 +1,6 @@
 package io.solar.security;
 
 import io.solar.entity.User;
-import io.solar.service.UserService;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
--- src/test/java/utils/TestResponsePage.java
+++ src/test/java/utils/TestResponsePage.java
@@ -13,9 +13,7 @@ public class TestResponsePage<T> extends PageImpl<T> {
 
     @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
     public TestResponsePage(@JsonProperty("content") List<T> content, @JsonProperty("number") int number, @JsonProperty("size") int size,
-                            @JsonProperty("totalElements") Long totalElements, @JsonProperty("pageable") JsonNode pageable, @JsonProperty("last") boolean last,
-                            @JsonProperty("totalPages") int totalPages, @JsonProperty("sort") JsonNode sort, @JsonProperty("first") boolean first,
-                            @JsonProperty("numberOfElements") int numberOfElements) {
+                            @JsonProperty("totalElements") Long totalElements, @JsonProperty("pageable") JsonNode pageable) {
         super(content, PageRequest.of(number, size), totalElements);
     }
 
