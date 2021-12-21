adaacccf4b8784faf7f9c45924553fc0f1d9fb93
Aliaksei Koran <akoran@scnsoft.com>
2021-12-21 14:50:15 +0300

    web Socket begining

---
+++ src/main/java/io/solar/controller/ChatController.java
@@ -0,0 +1,27 @@
+package io.solar.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ChatController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/chat")
+    public void processMessage(@Payload ChatMessage chatMessage) {
+
+        ChatMessage saved = chatMessageService.save(chatMessage);
+
+        messagingTemplate.convertAndSendToUser(
+                chatMessage.getRecipientId(),"/queue/messages",
+                new ChatNotification(
+                        saved.getId(),
+                        saved.getSenderId(),
+                        saved.getSenderName()));
+    }
+}
