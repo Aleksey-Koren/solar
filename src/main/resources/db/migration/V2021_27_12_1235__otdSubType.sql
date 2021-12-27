de52521558afc367bc42abbb6c943b491384acfb
Aliaksei Koran <akoran@scnsoft.com>
2021-12-27 12:51:01 +0300

    fix: OTD.sub_tupe DB migration

---
+++ src/main/resources/db/migration/V2021_27_12_1235__otdSubType.sql
@@ -0,0 +1,9 @@
+ALTER TABLE object_type_description MODiFY sub_type enum('STATIC', 'MINING', 'MILITARY', 'SCIENCE', 'PRODUCTION', 'ASYLUM', 'DYNAMIC') NULL;
+
+UPDATE object_type_description
+SET sub_type = 'STATIC'
+WHERE (description LIKE '%immobile%' AND type = 'STATION') OR title = 'Planet';
+
+UPDATE object_type_description
+SET sub_type = 'DYNAMIC'
+WHERE sub_type IS NULL;
