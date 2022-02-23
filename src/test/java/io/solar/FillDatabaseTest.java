c66923ab4fb841afd257434de4c6b1c4421e5918
avyshamirski <avyshamirski@scnsoft.com>
2022-02-23 18:00:15 +0300

    feat: implement generation prices/modifications

--- src/test/java/io/solar/FillDatabaseTest.java
+++ src/test/java/io/solar/FillDatabaseTest.java
@@ -2,7 +2,6 @@ package io.solar;
 
 
 import io.solar.dto.messenger.CreateRoomDto;
-import io.solar.entity.Goods;
 import io.solar.entity.Product;
 import io.solar.entity.User;
 import io.solar.entity.exchange.Exchange;
@@ -13,11 +12,21 @@ import io.solar.entity.marketplace.MarketplaceLot;
 import io.solar.entity.messenger.Message;
 import io.solar.entity.messenger.MessageType;
 import io.solar.entity.messenger.Room;
+import io.solar.entity.modification.Modification;
+import io.solar.entity.modification.ModificationPrice;
+import io.solar.entity.modification.ModificationType;
+import io.solar.entity.modification.ParameterModification;
+import io.solar.entity.modification.ParameterType;
 import io.solar.entity.objects.BasicObject;
-import io.solar.entity.objects.StarShip;
+import io.solar.entity.objects.ObjectTypeDescription;
+import io.solar.entity.objects.Station;
+import io.solar.entity.price.Price;
+import io.solar.entity.price.PriceProduct;
 import io.solar.facade.messenger.RoomFacade;
 import io.solar.repository.BasicObjectRepository;
-import io.solar.repository.StarShipRepository;
+import io.solar.repository.ObjectTypeDescriptionRepository;
+import io.solar.repository.ProductRepository;
+import io.solar.repository.StationRepository;
 import io.solar.repository.UserRepository;
 import io.solar.repository.exchange.ExchangeOfferRepository;
 import io.solar.repository.exchange.ExchangeRepository;
@@ -25,6 +34,12 @@ import io.solar.repository.marketplace.MarketplaceBetRepository;
 import io.solar.repository.marketplace.MarketplaceLotRepository;
 import io.solar.repository.messenger.MessageRepository;
 import io.solar.repository.messenger.RoomRepository;
+import io.solar.repository.modification.ModificationPriceRepository;
+import io.solar.repository.modification.ModificationRepository;
+import io.solar.repository.modification.ModificationTypeRepository;
+import io.solar.repository.modification.ParameterModificationRepository;
+import io.solar.repository.price.PriceProductRepository;
+import io.solar.repository.price.PriceRepository;
 import io.solar.security.Role;
 import io.solar.service.UserService;
 import net.bytebuddy.utility.RandomString;
@@ -61,10 +76,18 @@ public class FillDatabaseTest {
     private final MarketplaceBetRepository marketplaceBetRepository;
     private final BasicObjectRepository basicObjectRepository;
     private final ExchangeRepository exchangeRepository;
-    private final StarShipRepository starShipRepository;
+    private final PriceRepository priceRepository;
     private final ExchangeOfferRepository exchangeOfferRepository;
+    private final ProductRepository productRepository;
     private final RoomFacade roomFacade;
     private final UserService userService;
+    private final PriceProductRepository priceProductRepository;
+    private final ModificationTypeRepository modificationTypeRepository;
+    private final ModificationRepository modificationRepository;
+    private final ParameterModificationRepository parameterModificationRepository;
+    private final ModificationPriceRepository modificationPriceRepository;
+    private final ObjectTypeDescriptionRepository objectTypeDescriptionRepository;
+    private final StationRepository stationRepository;
 
     @Autowired
     public FillDatabaseTest(UserRepository userRepository,
@@ -72,23 +95,39 @@ public class FillDatabaseTest {
                             MessageRepository messageRepository,
                             MarketplaceBetRepository marketplaceBetRepository,
                             ExchangeRepository exchangeRepository,
+                            PriceRepository priceRepository,
                             ExchangeOfferRepository exchangeOfferRepository,
                             UserService userService,
                             MarketplaceLotRepository marketplaceLotRepository,
                             BasicObjectRepository basicObjectRepository,
-                            StarShipRepository starShipRepository,
-                            RoomFacade roomFacade) {
+                            ProductRepository productRepository,
+                            RoomFacade roomFacade,
+                            PriceProductRepository priceProductRepository,
+                            ModificationTypeRepository modificationTypeRepository,
+                            ModificationRepository modificationRepository,
+                            ParameterModificationRepository parameterModificationRepository,
+                            ModificationPriceRepository modificationPriceRepository,
+                            ObjectTypeDescriptionRepository objectTypeDescriptionRepository,
+                            StationRepository stationRepository) {
         this.userRepository = userRepository;
         this.roomRepository = roomRepository;
         this.messageRepository = messageRepository;
         this.marketplaceBetRepository = marketplaceBetRepository;
         this.exchangeRepository = exchangeRepository;
+        this.priceRepository = priceRepository;
         this.exchangeOfferRepository = exchangeOfferRepository;
         this.userService = userService;
         this.marketplaceLotRepository = marketplaceLotRepository;
         this.basicObjectRepository = basicObjectRepository;
-        this.starShipRepository = starShipRepository;
+        this.productRepository = productRepository;
         this.roomFacade = roomFacade;
+        this.priceProductRepository = priceProductRepository;
+        this.modificationTypeRepository = modificationTypeRepository;
+        this.modificationRepository = modificationRepository;
+        this.parameterModificationRepository = parameterModificationRepository;
+        this.modificationPriceRepository = modificationPriceRepository;
+        this.objectTypeDescriptionRepository = objectTypeDescriptionRepository;
+        this.stationRepository = stationRepository;
     }
 
     @Order(1)
@@ -164,21 +203,16 @@ public class FillDatabaseTest {
     @RepeatedTest(1)
     public void createExchanges() {
         List<User> users = userRepository.findAll();
-
-        int iteration = 0;
-        while (iteration < users.size() - 1) {
-
+        for (int i = 0; i < users.size() - 1; i += 2) {
             exchangeRepository.save(
                     Exchange.builder()
-                            .firstUser(users.get(iteration))
-                            .secondUser(users.get(iteration + 1))
+                            .firstUser(users.get(i))
+                            .secondUser(users.get(i + 1))
                             .startTime(Instant.now())
                             .firstAccepted(false)
                             .secondAccepted(false)
                             .build()
             );
-
-            iteration += 2;
         }
     }
 
@@ -199,6 +233,78 @@ public class FillDatabaseTest {
 
         exchangeOfferRepository.save(offer);
     }
+
+    @Order(8)
+    @RepeatedTest(100)
+    public void createPrices() {
+        Price price = Price.builder()
+                .moneyAmount(RANDOM.nextLong(1, 100_000))
+                .build();
+
+        price.setOwners(List.of(findRandomUser(), findRandomUser(), findRandomUser(), findRandomUser(), findRandomUser()));
+
+        priceRepository.save(price);
+    }
+
+    @Order(9)
+    @RepeatedTest(200)
+    public void createPriceProducts() {
+        PriceProduct priceProduct = PriceProduct.builder()
+                .product(findRandomProduct())
+                .price(findRandomPrice())
+                .productAmount(RANDOM.nextInt(1, 1000))
+                .build();
+
+        priceProductRepository.save(priceProduct);
+    }
+
+    @Order(10)
+    @RepeatedTest(100)
+    public void createModificationTypes() {
+        ModificationType modificationType = ModificationType.builder()
+                .title(RandomString.make())
+                .build();
+
+        modificationTypeRepository.save(modificationType);
+    }
+
+    @Order(11)
+    @RepeatedTest(100)
+    public void createModifications() {
+        Modification modification = Modification.builder()
+                .modificationType(findRandomModificationType())
+                .level((byte) RANDOM.nextInt(1, 5))
+                .description(RandomString.make())
+                .availableObjectTypeDescriptions(List.of(findRandomOTD(), findRandomOTD(), findRandomOTD(), findRandomOTD()))
+                .build();
+
+        modificationRepository.save(modification);
+    }
+
+    @Order(12)
+    @RepeatedTest(100)
+    public void createModificationPrices() {
+        ModificationPrice modificationPrice = ModificationPrice.builder()
+                .station(findRandomStation())
+                .modification(findRandomModification())
+                .price(findRandomPrice())
+                .build();
+
+        modificationPriceRepository.save(modificationPrice);
+    }
+
+    @Order(13)
+    @RepeatedTest(100)
+    public void createParameterModifications() {
+        ParameterModification parameterModification = ParameterModification.builder()
+                .modificationValue(RANDOM.nextDouble(1.0, 100.0))
+                .modification(findRandomModification())
+                .parameterType(ParameterType.values()[RANDOM.nextInt(0, ParameterType.values().length - 1)])
+                .build();
+
+        parameterModificationRepository.save(parameterModification);
+    }
+
 
     private User findRandomUser() {
         List<User> users = userRepository.findAll();
@@ -228,5 +334,41 @@ public class FillDatabaseTest {
         List<Exchange> exchanges = exchangeRepository.findAll();
 
         return exchanges.get(RANDOM.nextInt(0, exchanges.size()));
+    }
+
+    private Price findRandomPrice() {
+        List<Price> prices = priceRepository.findAll();
+
+        return prices.get(RANDOM.nextInt(0, prices.size()));
+    }
+
+    private Product findRandomProduct() {
+        List<Product> products = productRepository.findAll();
+
+        return products.get(RANDOM.nextInt(0, products.size()));
+    }
+
+    private ModificationType findRandomModificationType() {
+        List<ModificationType> modificationTypes = modificationTypeRepository.findAll();
+
+        return modificationTypes.get(RANDOM.nextInt(0, modificationTypes.size()));
+    }
+
+    private ObjectTypeDescription findRandomOTD() {
+        List<ObjectTypeDescription> all = objectTypeDescriptionRepository.findAll();
+
+        return all.get(RANDOM.nextInt(0, all.size()));
+    }
+
+    private Modification findRandomModification() {
+        List<Modification> modifications = modificationRepository.findAll();
+
+        return modifications.get(RANDOM.nextInt(0, modifications.size()));
+    }
+
+    private Station findRandomStation() {
+        List<Station> stations = stationRepository.findAll();
+
+        return stations.get(RANDOM.nextInt(0, stations.size()));
     }
 }
