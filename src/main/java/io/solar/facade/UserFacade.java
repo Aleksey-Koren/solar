1ed52bb27f0140ec9b21dcc02fd0422b383c7df3
avyshamirski <avyshamirski@scnsoft.com>
2022-05-13 17:25:03 +0300

    refactor: implement permissions to user dto

--- src/main/java/io/solar/facade/UserFacade.java
+++ src/main/java/io/solar/facade/UserFacade.java
@@ -4,17 +4,20 @@ import io.solar.dto.UserDto;
 import io.solar.entity.Permission;
 import io.solar.entity.User;
 import io.solar.entity.messenger.NotificationType;
+import io.solar.mapper.PermissionMapper;
 import io.solar.mapper.UserMapper;
 import io.solar.mapper.object.BasicObjectViewMapper;
 import io.solar.service.UserService;
 import io.solar.service.engine.interfaces.NotificationEngine;
 import io.solar.service.messenger.RoomService;
+import io.solar.service.object.BasicObjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -22,7 +25,8 @@ public class UserFacade {
 
     private final UserService userService;
     private final UserMapper userMapper;
-    private final BasicObjectViewMapper basicObjectViewMapper;
+    private final PermissionMapper permissionMapper;
+    private final BasicObjectService basicObjectService;
     private final RoomService roomService;
     private final NotificationEngine notificationEngine;
 
@@ -39,7 +43,8 @@ public class UserFacade {
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Couldn't found user with such id"));
         user.setTitle(dto.getTitle());
         user.setMoney(dto.getMoney());
-        user.setLocation(basicObjectViewMapper.toEntity(dto.getLocation()));
+        user.setLocation(basicObjectService.getById(dto.getLocationId()));
+        user.setPermissions(dto.getPermissions().stream().map(permissionMapper::toEntity).collect(Collectors.toSet()));
         userService.update(user);
         return userMapper.toDto(user);
     }
