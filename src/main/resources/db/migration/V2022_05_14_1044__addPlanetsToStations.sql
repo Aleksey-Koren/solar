fd3fbe79c5ad3c38ee308c9e803a2a75e529317a
Aliaksei Koran <akoran@scnsoft.com>
2022-05-14 10:45:46 +0300

    feat: add DB migration. Add planets to stations

---
+++ src/main/resources/db/migration/V2022_05_14_1044__addPlanetsToStations.sql
@@ -0,0 +1,19 @@
+UPDATE solar.objects t
+SET t.planet = 505
+WHERE t.id = 11;
+
+UPDATE solar.objects t
+SET t.planet = 496
+WHERE t.id = 4;
+
+UPDATE solar.objects t
+SET t.planet = 496
+WHERE t.id = 6;
+
+UPDATE solar.objects t
+SET t.planet = 496
+WHERE t.id = 5;
+
+UPDATE solar.objects t
+SET t.planet = 506
+WHERE t.id = 15;
