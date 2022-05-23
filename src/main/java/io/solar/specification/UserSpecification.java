85b55c2f31a694e6c29b96f1da9d7bb4a111167d
Aliaksei Koran <akoran@scnsoft.com>
2022-05-23 10:21:39 +0300

    feat: User specification NOtInRoom

--- src/main/java/io/solar/specification/UserSpecification.java
+++ src/main/java/io/solar/specification/UserSpecification.java
@@ -1,15 +1,19 @@
 package io.solar.specification;
 
-import io.solar.specification.filter.UserFilter;
 import io.solar.entity.User;
 import io.solar.entity.User_;
+import io.solar.entity.messenger.Room;
+import io.solar.entity.messenger.Room_;
+import io.solar.specification.filter.UserFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.domain.Specification;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import javax.persistence.criteria.Subquery;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,12 +26,23 @@ public class UserSpecification implements Specification<User> {
     @Override
     public Predicate toPredicate(Root<User> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
         List<Predicate> predicateList = new ArrayList<>();
+
         if (filter.getLogin() != null) {
             predicateList.add(criteriaBuilder.like(root.get(User_.login), filter.getLogin() + "%"));
         }
+
         if (filter.getTitle() != null) {
             predicateList.add(criteriaBuilder.like(root.get(User_.title), filter.getTitle() + "%"));
         }
+
+        if (filter.getNotInRoom() != null) {
+            Subquery<User> userSubQuery = query.subquery(User.class);
+            Root<User> subQueryRoot = userSubQuery.from(User.class);
+            Join<User, Room> userRoomJoin = subQueryRoot.join(User_.rooms);
+            userSubQuery.select(subQueryRoot).where(criteriaBuilder.equal(userRoomJoin.get(Room_.id), filter.getNotInRoom()));
+            predicateList.add(criteriaBuilder.not(criteriaBuilder.in(root).value(userSubQuery)));
+        }
+
         return criteriaBuilder.and(predicateList.toArray(new Predicate[0]));
     }
 }
--- src/main/java/io/solar/specification/filter/UserFilter.java
+++ src/main/java/io/solar/specification/filter/UserFilter.java
@@ -11,5 +11,5 @@ public class UserFilter {
 
     private String login;
     private String title;
-    private Integer notInRoom;
+    private Long notInRoom;
 }
